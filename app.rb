c25d9a9f26fa21181fd6609f881bc3a16bd4fbc5
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-26 14:11:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end 
+  
+  get '/name' do 
+    "My name is Lily Byrne"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Silver Spring, MD"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Don't Rain on my Parade or Nice for What"
+  end 
+  
 end
